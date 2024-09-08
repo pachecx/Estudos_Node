127c24d93a7b4f6e69d70278abdb10b86421dec1
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-09-08 10:47:14 -0300

    nodeMon

--- Express/Install/index.js
+++ Express/Install/index.js
@@ -1,7 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 300
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Olá Mundo!')
-})
+const path = require("path");
+
+const basePath = path.join(__dirname, "Templates");
+
+const checkAuth = function (req, res, next){
+    req.authStatus = true
+
+    if(req.authStatus){
+        console.log('Logado')
+        next()
+    }else{
+        console.log('Não Logado!')
+    }
+}
+
+app.use(checkAuth)
+
+app.get("/", (req, res) => {
+  res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`App rodando na porta ${port}`);
+});
