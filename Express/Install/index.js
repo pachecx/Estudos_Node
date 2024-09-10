a0a0a6b7f7a5d5385a4325e19a06acbb446bfdd7
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-09-09 21:32:21 -0300

    up

--- Express/Install/index.js
+++ Express/Install/index.js
@@ -4,20 +4,37 @@ const port = 3000;
 
 const path = require("path");
 
+
 const basePath = path.join(__dirname, "Templates");
 
-const checkAuth = function (req, res, next){
-    req.authStatus = true
+const checkAuth = function (req, res, next) {
+  req.authStatus = true;
 
-    if(req.authStatus){
-        console.log('Logado')
-        next()
-    }else{
-        console.log('Não Logado!')
-    }
-}
+  if (req.authStatus) {
+    console.log("Logado");
+    next();
+  } else {
+    console.log("Não Logado!");
+  }
+};
 
-app.use(checkAuth)
+app.use(checkAuth);
+
+app.get("/user/add", (req, res) => {
+  res.sendFile(`${basePath}/userform.html`);
+});
+
+app.post("/users/save", (req, res) => {
+  console.log(req.body);
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  console.log(`Novo usuario ${id}`);
+
+  res.sendFile(`${basePath}/users.html`);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(`${basePath}/index.html`);
