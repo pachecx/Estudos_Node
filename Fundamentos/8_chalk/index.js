2036b57cdd850b7ae3e335bd5d9df52a60f9afb3
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-10-14 16:04:30 -0300

    commit local

---
+++ Fundamentos/8_chalk/index.js
@@ -0,0 +1,16 @@
+import chalk from "chalk";
+//const chalk = require("chalk");
+
+const nota = 10;
+
+console.log(chalk.green("Aprovado!!"));
+
+const nota2 = 2
+
+// if(nota2 < 7){
+//     console.log(chalk.bgRed("Reprovado"))
+// }else{
+//     console.log(chalk.green('Aprovado!!'))
+// }
+
+console.log((chalk.red('relou!!')))
