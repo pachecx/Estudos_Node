535cd8aec29bc1856baa7f5aa8477b6c5b4d70bb
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-10-15 16:35:19 -0300

    coremodule

--- Core_modules/4_Http_com_Url/index.js
+++ Core_modules/4_Http_com_Url/index.js
@@ -1,11 +1,10 @@
 const http = require("http");
-const url = require("url")
 
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-
-    const urlInfo = require('url').parse(req.url, true)
+  const urlInfo = require("url").parse(req.url, true);
+  const name = urlInfo.query.name
   res.statusCode = 200;
   res.setHeader("Contenty-Type", "text/html");
   res.end("<h1>Olá, primeiro serve com html!!</h1>");
