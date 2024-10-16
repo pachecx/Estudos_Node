4fb4b89e4adf8cacd308c325c9be26fcd83b28c7
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-10-16 17:02:18 -0300

    finalizando core modulos

---
+++ Core_modules/14_diretorio/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+if(!fs.existsSync('./minhapasta')){
+    console.log('nao existe!')
+}
+
+fs.mkdirSync('minhapasta')
+
+
+if(fs.existsSync('./minhapasta')){
+    console.log('existe!')
+}
