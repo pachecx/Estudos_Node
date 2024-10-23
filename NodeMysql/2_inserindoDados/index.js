db1641c421e866a660da32c2ff8ea675fc0f37da
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-10-23 18:49:47 -0300

    up

--- NodeMysql/2_inserindoDados/index.js
+++ NodeMysql/2_inserindoDados/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
 
 const hbs = exphbs.create({
   partialsDir: ["views/partials"],
@@ -13,15 +21,29 @@ app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
-
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.post('/books/insertbook', (req, res) => {
+  const title = req.body.title
+  const page = req.body.page
+
+
+  const query = `INSERT INTO books (title, page) VALUES ('${title}', '${page}')`
+  conn.query(query, function(err){
+    if(err){
+      console.log(err)
+    }
+
+    res.redirect('/')
+  })
+})
+
 const conn = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "123456",
   database: "nodemysql2",
 });
 
