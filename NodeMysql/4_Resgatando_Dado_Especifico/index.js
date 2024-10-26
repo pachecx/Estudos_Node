0ac44ff85a36d6ef8929e92762a1ddf218f88114
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-10-25 23:48:08 -0300

    up

---
+++ NodeMysql/4_Resgatando_Dado_Especifico/index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql2");
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+const hbs = exphbs.create({
+  partialsDir: ["views/partials"],
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.post("/books/insertbook", (req, res) => {
+  const title = req.body.title;
+  const page = req.body.page;
+
+  const query = `INSERT INTO books (title, page) VALUES ('${title}', '${page}')`;
+  conn.query(query, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.redirect("/books");
+  });
+});
+
+app.get("/books", (req, res) => {
+  const sql = "SELECT * FROM books";
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const books = data;
+
+    console.log(books);
+
+    res.render("books", { books });
+  });
+});
+
+app.get("/books/:id", (req, res) => {
+  const id = req.params.id;
+
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const book = data[0];
+
+    res.render("book", { book });
+  });
+});
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "nodemysql2",
+});
+
+conn.connect(function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("MySql conectado!");
+  app.listen(3000);
+});
